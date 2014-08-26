7e19c0f6be6951ac0afb7a142e0a9965a96d31af
bstrasser <bernhard.strasser@meduniwien.ac.at>
2014-08-26 10:40:05 +0200

    Unknown Changes.

--- make_basis.sh
+++ make_basis.sh
@@ -1,10 +1,16 @@
-#####################################################################
-###  Create .basis-files for LCModel out of jmRUI-txt-files   #######
-#####################################################################
+#################################################################################
+#######  Create .basis-files for LCModel fitting out of jmRUI-txt-files   #######
+#################################################################################
 
-######### make sure that there are no other txt-files in the folder you give to  the program except the ones that should be included to the basis set and the TMS-reference-file
-######### the .txt-files have to include the metabolite names: the program searches for:
-######### 'asp','cr','gaba','gln','glu','gly','gpc','lac','lip_c','mI','ins','mm3','mm4','naa','naag','pch','pcr','scyllo','tau'
+######### This program creates .RAW-files out of simulated .txt-files using jmRUI and files for LCM-procession (makebasis.in-files). These files get then processed by LCM which creates .basis and .ps-files.
+######### The .basis-files can then be used as a basis in LCM for metabolite fitting. You can change the acquisition delay (for a FID-sequence).
+
+
+######### You can either parse the name of the directory with all the .txt-files or give the program all the names with the .txt-files.
+######### Make sure that there are no other txt-files in the folder you give to  the program except the ones that should be included to the basis set and the TMS-reference-file.
+######### If you give individual .txt-files to the program or several acq-delays, put them in qoutation marks ".
+######### The .txt-files have to include the metabolite names in their names. The program searches for:
+######### 'ala','asp','cho','cr','gaba','glc','gln','glu','gly','gpc','lac','lip_c','mI','ins','mm3','mm4','naa','naag','pch','pcr','scyllo','tau' (case-insensitive)
 
 echo -e "\n\n   START"
 
@@ -60,7 +66,6 @@ shift $(($OPTIND - 1))
 #fi
 
 
-
 #2.
 ################## WRITE INFO TO tmp.txt  ########################
 echo -e "\nWrite user input to temp file!"
@@ -84,23 +89,17 @@ echo -e "\n\nRead out input.txt-files, create .RAW-files and write makebasis.in"
 rm -r $out_dir
 m78 -nodisplay -nojvm < make_basis_txt2raw.m
 
-echo -e "\nLCmodel processing started!"
+echo -e "\n\nLCmodel processing started!\n"
 
 OutDir_list=$(find $out_dir/*ms -type d)
-#echo $OutDir_list
-#exclude=1
 
 for out_sub_dir in $OutDir_list ; do
-#	if [ $exclude -ge 2 ]; then
-#		echo $out_sub_dir
-		$HOME/.lcmodel/bin/makebasis < $out_sub_dir/makebasis*.in
-#		echo "$HOME/.lcmodel/bin/makebasis < $out_sub_dir/makebasis*"
-#	fi
-#	let exclude=$exclude+1		
+	echo "Processing $out_sub_dir"
+	$HOME/.lcmodel/bin/makebasis < $out_sub_dir/makebasis*.in
 done
 
 
-#rm -r $tmp
+#rm -r ./tmp
 echo -e "\n\n   END"
 
 
--- run_make_basis.sh
+++ run_make_basis.sh
@@ -1,9 +1,12 @@
-cd /data/mrsproc/PROCESS_Bernhard/Make_Basis_jmrui_to_basis
 chmod 755 ./*.sh
 chmod 755 ./*.m
-#make_basis.sh -i "./mI_7T.txt ./asp_7T.txt naa_7T.txt" -o ./out -a 13.3333333
 
-#make_basis.sh -i ./met_sim_30000Hz/tx -o ./out -a 1.333333 -r ./met_sim_30000Hz/tx/dss_7T.txt
+make_basis.sh -i ./met_sim_30000Hz/txt -o ./out -a "1.333333 1.666666 2.0 2.333333 2.666666 3.0 3.333333 3.666666 4.0 4.333333 4.666666 5.0 5.333333 5.666666 6.0 7.333333 7.666666 8.0 8.333333 8.666666 9.0 9.333333 9.666666 10.0"  -r ./met_sim_30000Hz/txt/dss_7T.txt
 
-make_basis.sh -i ./met_sim_30000Hz/fid_varTE -o ./outx -a 0.0 -r ./met_sim_30000Hz/fid_varTE/dss.txt
+
+#make_basis.sh -i ./met_sim_30000Hz/txt -o ./out -a 4.0  -r ./met_sim_30000Hz/txt/dss_7T.txt
+
+#not working: 7.666666 4.0
+#8.0 8.333333 8.666666 9.0 9.333333 9.666666 10.0"
+#make_basis.sh -i ./met_sim_30000Hz/fid_varTE -o ./outx -a 1.333333333 -r ./met_sim_30000Hz/fid_varTE/dss.txt
 
